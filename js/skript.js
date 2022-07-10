e0a74e8d621be5b91a2a614b11b484b4c9465fd2
maudzydun <semenzelenskij@gmail.com>
2022-07-10 13:29:10 +0300

    Додали функціоналу кнопкам вибору який сайт потрібен

--- js/skript.js
+++ js/skript.js
@@ -1,5 +1,48 @@
+/////////////////////////////////////////////////////////////////////////
 function hideContactBlock() {
     $('.hiden-contact-form').slideToggle(600);
+    $('.hiden-contact-form_main-block textarea').val("");
 };
 
 $('.hide-contact-btn').on('click', hideContactBlock);
+//Функція і обробник івентів для прихованного меню для зворотнього звязку
+
+/////////////////////////////////////////////////////////////////
+function hideServiceBlock() {
+    $('.hiden-services').css({
+        'display': 'flex'
+    });
+    $('.services-main_block').css({
+        'border-radius': '0 0 15px 15px',
+    });
+};
+
+$('.btn-for-services').on('click', hideServiceBlock);
+//Блок який відображає на сайті додатковий блок в секції сервиси
+
+///////////
+function showLandingContactBlock () {
+    $('.hiden-contact-form').slideToggle(600);
+    $('.hiden-contact-form_main-block textarea').val("Мені потрібен Лендінг. Зв'яжіться зі мною найближчим часом");
+};
+
+$('#lending-button').on('click', showLandingContactBlock);
+// Лендінг
+
+///////////////////
+function showIMContactBlock () {
+    $('.hiden-contact-form').slideToggle(600);
+    $('.hiden-contact-form_main-block textarea').val("Мені потрібен Інтернет магазин. Зв'яжіться зі мною найближчим часом");
+};
+
+$('#internet-shop-button').on('click', showIMContactBlock);
+// Інтернет магазин
+
+/////////////////////
+function showCorporationContactBlock () {
+    $('.hiden-contact-form').slideToggle(600);
+    $('.hiden-contact-form_main-block textarea').val("Мені потрібен Корпоративний сайт. Зв'яжіться зі мною найближчим часом");
+};
+
+$('#korporation-button').on('click', showCorporationContactBlock);
+// корпораьтвний сайт
